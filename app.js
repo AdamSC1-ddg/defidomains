78041d38df6efb6683cbfecf61f67dfec7e27d41
AdamSC1-ddg <adamscottcochran@gmail.com>
2020-08-17 11:09:14 -0400

    v2
    v2

--- app.js
+++ app.js
@@ -1,11 +1,70 @@
-var express = require('express');
-var app = express();
+var http = require('http');
+var fs = require('fs');
 var path = require('path');
-const Eth = require('ethjs');
-const eth = new Eth(new Eth.HttpProvider('https://rinkeby.infura.io/v3/c3eb562930cb47c5bb3f6c597b9416c1'));
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
-app.listen(8081, function () {
-  console.log('Running on port 8081');
-});
+
+http.createServer(function (request, response) {
+    console.log('request starting...');
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+			
+// Website you wish to allow to connect
+response.setHeader('Access-Control-Allow-Origin', '*');
+
+// Request methods you wish to allow
+response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+// Request headers you wish to allow
+response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+// Set to true if you need the website to include cookies in the requests sent
+// to the API (e.g. in case you use sessions)
+response.setHeader('Access-Control-Allow-Credentials', true);
+
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(8081);
+console.log('Server running at http://127.0.0.1:8125/');
